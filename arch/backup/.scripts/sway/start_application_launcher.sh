0a419dca1b36bd4ad0bdb96a201ca68a0b550198
infernotnt <infernotnt@gmail.com>
2021-04-26 14:47:44 +0200

    c

--- arch/backup/.scripts/sway/start_application_launcher.sh
+++ arch/backup/.scripts/sway/start_application_launcher.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# If you application launcher is not called "alacritty --title application_launcher...." then you will need to modify this file.
+
 TERMINAL="alacritty"
 
 # Starts an "application launcher", that is actually just clever use of fzf
 
-output1=$(ps -ef | grep 'application_launcher' | grep -vc -e 'grep application_launcher' -e 'start_application_launcher.sh')
+output1=$(ps -ef | grep 'alacritty --title application_launcher.*' | grep -v 'grep alacritty --title application_launcher.*') # List of processes that are assumed to be the apllication launcher
 
-echo "EVO GA ${output1}"
-if [ "${output1}" == "1" ]
+number=$(printf "$output1" | wc -w)
+
+if [ "${number}" -gt 1 ]
      then
-          ps -ef | grep 'application_launcher' | grep -v -e 'grep application_launcher' -e 'start_application_launcher.sh' | awk '{print $2}' | xargs -r kill
+          printf "$output1" | awk '{print $2}' | xargs -r kill
      else
           $TERMINAL --title application_launcher -e bash -c 'compgen -c | grep -v fzf | sort -u | fzf --layout=reverse | xargs -r swaymsg -t command exec'
 fi

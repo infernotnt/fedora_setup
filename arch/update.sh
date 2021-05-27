6dc8490e77dc29ed308adddf43556d3cceb0a954
infernotnt <infernotnt@gmail.com>
2021-05-27 10:41:49 +0200

    c

--- arch/update.sh
+++ arch/update.sh
@@ -8,11 +8,17 @@ fi
 
 sudo rm -vr ./backup
 
+#--------------
+
 mkdir -pv backup/.config
 mkdir -pv backup/.scripts
 mkdir -pv backup/.local/share/fonts
 mkdir -pv backup/wallpaper
+
 mkdir -pv backup/.config/emacs
+mkdir -pv backup/.config/emacs/my_templates_ms
+
+#--------------
 
 sudo cp -rvp ~/.config/sway backup/.config/sway
 sudo cp -rvp ~/.config/waybar backup/.config/waybar
@@ -20,8 +26,10 @@ sudo cp -rvp ~/.config/waybar backup/.config/waybar
 sudo cp -rvp ~/.config/zsh backup/.config/zsh
 sudo cp -rvp ~/.config/nvim backup/.config/nvim
 sudo cp -rvp ~/.config/git backup/.config/git
+
 sudo cp -rvp ~/.config/emacs/init.el backup/.config/emacs/init.el
 sudo cp -rvp ~/.config/emacs/config.org backup/.config/emacs/config.org
+sudo cp -rvp ~/.config/emacs/my_templates_ms backup/.config/emacs/my_templates_ms
 
 sudo cp -rvp ~/.scripts/* backup/.scripts/
 sudo cp -vp ~/.local/share/fonts/* backup/.local/share/fonts
